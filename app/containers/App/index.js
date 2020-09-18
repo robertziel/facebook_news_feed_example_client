5a178dcd19455a19635cc41d5de18479e1d4fcea
Robert Zieliński <hello@robertz.co>
2020-09-18 19:15:40 +0200

    Wrapp App in Container

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -10,12 +10,13 @@
 import React from 'react';
 import { Redirect, Switch, Route } from 'react-router-dom';
 
-import { Scroll } from 'components/_ui-elements';
+import { Container, Scroll } from 'components/_ui-elements';
 
 import Navbar from 'components/Navbar/index';
 import Footer from 'components/Footer/index';
 
 import HomePage from 'containers/_pages/HomePage/Loadable';
+import PostPage from 'containers/_pages/PostPage/Loadable';
 import ProfilePage from 'containers/_pages/ProfilePage/Loadable';
 
 import ContentWrapper from './ContentWrapper';
@@ -24,15 +25,18 @@ function App() {
   return (
     <ContentWrapper>
       <Navbar />
-      <Scroll>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/profile" component={ProfilePage} />
-          <Route>
-            <Redirect to="/" />
-          </Route>
-        </Switch>
-      </Scroll>
+      <Container>
+        <Scroll>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/post/:id" component={PostPage} />
+            <Route exact path="/profile" component={ProfilePage} />
+            <Route>
+              <Redirect to="/" />
+            </Route>
+          </Switch>
+        </Scroll>
+      </Container>
       <Footer />
     </ContentWrapper>
   );
